8a79912074e0bff7d4296f738f2987908c1ad68f
Dipra Das <dipradas5940@gmail.com>
2022-10-12 23:32:43 +0600

    Done

--- src/components/TestAnswer/TestAnswer.js
+++ src/components/TestAnswer/TestAnswer.js
@@ -2,15 +2,34 @@ import React, { useState } from 'react';
 import './TestAnswer.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEye } from '@fortawesome/free-solid-svg-icons'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const TestAnswer = ({ quiz }) => {
+
     const [show, setShow] = useState(false);
+        const showToastMessage = (myAns) => {
+            if (myAns === quiz.correctAnswer) {
+                toast.success('Right Answer !', {
+                    position: toast.POSITION.TOP_CENTER
+                });
+            }
+            else {
+                toast.error('Wrong Answer !', {
+                    position: toast.POSITION.TOP_CENTER
+                });
+            }
+        };
+
     return (
         <div className='qquiz'>
             <h3>{quiz.question}</h3>
             <div className="quizOption">
                 {quiz.options.map(eachOptions =>
-                    <h6>{eachOptions}</h6>
+                    <div>
+                        <h6 onClick={() => showToastMessage(eachOptions)}>{eachOptions}</h6>
+                        <ToastContainer />
+                    </div>
                 )}
             </div>
             <div onClick={() => setShow(!show)}><FontAwesomeIcon icon={faEye} /></div>
